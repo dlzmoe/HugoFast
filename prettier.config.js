d92bca47eaae44530000e2462c6337e2b6e55c12
WRXinYue <3322543587@qq.com>
2023-06-09 23:19:11 +0800

    🪄 fix prettier code formatting

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+};
